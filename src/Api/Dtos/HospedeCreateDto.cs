dec36fa057d0c23a5da02939233911af24398f4e
Pedro Henrique <phdoliwa7@gmail.com>
2026-05-05 20:25:06 -0300

    DTOs hospede

---
+++ src/Api/Dtos/HospedeCreateDto.cs
@@ -0,0 +1,9 @@
+namespace PousadaApi.Api.Dtos;
+
+public class HospedeCreateDto
+{
+    public string Nome { get; set; } = string.Empty;
+    public string? Telefone { get; set; }
+    public string? Email { get; set; }
+    public string? Documento { get; set; }
+}
